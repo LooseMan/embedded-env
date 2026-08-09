8762952b483fa9dc77bac19451cbd4ab7f3b3b3d
LooseMan <46617161+LooseMan@users.noreply.github.com>
2026-08-09 20:29:38 +0900

    feat: Ansible用のDockerfileとスクリプトを更新し、パッケージインストール手順を改善

--- ansible/container-run.sh
+++ ansible/container-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE=verify
+IMAGE=ansible
 
 # docker コマンドがない場合は podman に処理を転送する関数を作る
 if ! command -v docker &> /dev/null; then
@@ -9,9 +9,11 @@ if ! command -v docker &> /dev/null; then
     }
 fi
 
+# host ネットワークを使うため、--network host を指定する
+# -w /workspace は、コンテナ内の作業ディレクトリをホスト側のカレントディレクトリに合わせるため
 docker run --rm -it \
-    --cap-add=SYS_PTRACE \
-    --security-opt seccomp=unconfined \
+    --network host \
     -v "$(pwd)":/workspace:Z \
     -v "$HOME/.ssh":/root/.ssh:Z \
+    -w /workspace \
     "$IMAGE" "$@"
